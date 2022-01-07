d1a9d8498f25151b4adefa66746ad4119231eab7
monginadiana <diana.ondieki@student.moringa.com>
2022-01-07 14:59:05 +0300

    addition of the join and leave hood functions in the views.py

--- app/views.py
+++ app/views.py
@@ -12,20 +12,20 @@ from app.models import Profile
 def index(request):
     return render(request, 'index.html')
 
-@login_required(login_url='/accounts/login/')
-def create_profile(request):
-    current_user = request.user
-    title = "Create Profile"
-    if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = current_user
-            profile.save()
-        return HttpResponseRedirect('/')
-    else:
-        form = ProfileForm()
-    return render(request, 'create_profile.html', {"form": form, "title": title})
+# @login_required(login_url='/accounts/login/')
+# def create_profile(request):
+#     current_user = request.user
+#     title = "Create Profile"
+#     if request.method == 'POST':
+#         form = ProfileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             profile = form.save(commit=False)
+#             profile.user = current_user
+#             profile.save()
+#         return HttpResponseRedirect('/')
+#     else:
+#         form = ProfileForm()
+#     return render(request, 'create_profile.html', {"form": form, "title": title})
 
 @login_required(login_url="/accounts/login/")
 def profile(request):
