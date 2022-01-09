8cbf91147b5af1deaea933b8c2d71e1e73b9c4f3
monginadiana <diana.ondieki@student.moringa.com>
2022-01-10 01:05:47 +0300

    modification of the registration.html

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
-from .models import Profile, NeighbourHood, Location
+from .models import Business, Post, Profile, NeighbourHood, Location
 
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(NeighbourHood)
 admin.site.register(Location)
+admin.site.register(Business)
+admin.site.register(Post)
